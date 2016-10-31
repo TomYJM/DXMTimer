611c96f1555881d95ab6fc191438a52dcc2cd70b
BBJM <BIBOJM@BBJMdeMacBook-Pro.local>
2016-10-31 10:56:50 +0800

    first commit

---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by YANGJINMING on 16/3/28.
+//  Copyright © 2016年 YANGJINMING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
